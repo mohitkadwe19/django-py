0e07ed22ebd64ed02cdeb0b43b15487681782cd0
mohitkadwe19 <118180431+mohitkadwe@users.noreply.github.com>
2024-11-14 23:43:57 +0530

    📦 NEW: Add route for product addition with addProduct view

--- playground/urls.py
+++ playground/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', views.login),
     path('forgotPassword/', views.forgotPassword),
     path('profile/', views.profile),
+    path('addProduct/', views.addProduct),
 ]
